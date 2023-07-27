4ad8f92a05b55592590f6636fb5e0fa5868f3dbd
Nguyen Quang Hao <haolk4@gmail.com>
2023-07-27 17:25:34 +0900

    xây dựng logic phần header phần 1

--- src/components/AccountItem/index.js
+++ src/components/AccountItem/index.js
@@ -2,15 +2,16 @@ import classNames from 'classnames/bind';
 import styles from './AccountItem.module.scss';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCheckCircle } from '@fortawesome/free-solid-svg-icons';
+import Image from '~/components/Image';
 
 const cx = classNames.bind(styles);
 
 function AccountItem() {
     return (
         <div className={cx('wrapper')}>
-            <img
+            <Image
                 className={cx('avatar')}
-                src="https://p16-sign-sg.tiktokcdn.com/aweme/100x100/tos-alisg-avt-0068/829250894f9da59dbf31034fcf04e6b4.jpeg?x-expires=1690444800&x-signature=iavfhDPQPfFX8XeK48bVX%2F%2FxGxI%3D"
+                src="https://dp16-sign-sg.tiktokcdn.com/aweme/100x100/tos-alisg-avt-0068/829250894f9da59dbf31034fcf04e6b4.jpeg?x-expires=1690444800&x-signature=iavfhDPQPfFX8XeK48bVX%2F%2FxGxI%3D"
                 alt="hoa"
             />
             <div className={cx('info')}>
